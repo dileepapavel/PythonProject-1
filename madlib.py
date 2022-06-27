8808e249f530ee85bda6168a9a6cb5652cd5cf15
pavel98 <51260268+dileepapavel@users.noreply.github.com>
2022-06-27 16:59:20 +0530

    Initial commit was done

---
+++ madlib.py
@@ -0,0 +1,16 @@
+# This is my first Python Project.
+# Need to upload this into the GitHub later on
+
+# First we'll create variables
+adjective = input("Please input a adjective: ")
+verb1 = input("please input a Verb: ")
+verb2 = input("Please input a second Verb: ")
+personName = input("Please input a Persons Name: ")
+
+ # Next we'll create a madlib here
+madlib = f"I love programming because it is {adjective}.\
+ I always like to {verb1} and {verb2}.\
+ I am {personName}.!!"
+
+# Here we'll print the above statement
+print(madlib)
